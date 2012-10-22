ddc5f456f0c2412512a083a2cde3e6944494bd00
Thomas Sturges-Allard <zig13@hotmail.co.uk>
2012-10-22 01:49:31 +0100

    Commented most lines and added some initial processing of the inputted numbers

--- numbercruncher.py
+++ numbercruncher.py
@@ -3,11 +3,22 @@ from os import curdir, sep, access, R_OK
 dot = str(curdir) #The character used by the current os to denote the current folder. Is '.' in Windows
 sep = str(sep) #The character used by the current os to denote the demotion to another folder level. Is '/' in Windows
 print "What is the name of the file in which the data is located?"
-while True :
+while True : #Infinite loop
 	prompt = raw_input(">")
-	infile = dot+sep+prompt
-	if access(infile, R_OK) :
-		break
+	infile = dot+sep+prompt #Adds ./ in front of the filename to generate a filepath from the scripts directory
+	if access(infile, R_OK) : #Checks if given file can be accessed (safer than checking if path exists)
+		break #Breaks out of while loop if file can be accessed
 	else :
 		print "File not found"
-infile = open(infile, 'r')
+infile = open(infile, 'r') #Opens the given file
+indata = infile.readlines()
+indata = map(lambda s: s.strip(), indata) #Removes new lines
+if not indata[0].isdigit() : #Removed header if exists
+	del indata[0]
+try :
+	indata = map(int, indata) #Converts items in list to strings
+except ValueError, e : #If a non-number is found then:
+	print "File contains non-number characters"
+	raw_input("") #Used so that if script is run non-interactively, terminal/prompt will not imediately close and you can read the error message
+	exit(0) #Forces exit
+print indata
