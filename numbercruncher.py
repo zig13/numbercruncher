6ce03aacc3624b03d7af01462aa6f674951530e0
Thomas Sturges-Allard <zig13@hotmail.co.uk>
2012-10-22 03:09:09 +0100

    Range is printed and added some comments

--- numbercruncher.py
+++ numbercruncher.py
@@ -6,7 +6,11 @@ print "What is the name of the file in which the data is located?"
 while True : #Infinite loop
 	prompt = raw_input(">")
 	infile = dot+sep+prompt #Adds ./ in front of the filename to generate a filepath from the scripts directory
-	if access(infile, R_OK) : #Checks if given file can be accessed (safer than checking if path exists)
+	if prompt == "" : #If nothing put in prompt
+		infile = dot+sep+"numbers.txt" #For testing purposes - saves me typing it in every time
+		print infile
+		break
+	elif access(infile, R_OK) : #Checks if given file can be accessed (safer than checking if path exists)
 		break #Breaks out of while loop if file can be accessed
 	else :
 		print "File not found"
@@ -23,15 +27,15 @@ except ValueError, e : #If a non-number is found then:
 	exit(0) #Forces exit
 print indata, "\n"
 print "The lowest number is %s\nWould you like to change it?" %(min(indata))
-prompt = raw_input(">").lower()
-if (prompt == '1') or (prompt == 'y') or (prompt == 'yes') or (prompt == 'sure') or (prompt == 'true')  or (prompt == 'ja') :
+prompt = raw_input(">").lower() #Captures use input and puts the input into lowercase so 'Yes' will be recognised as 'yes'
+if (prompt == '1') or (prompt == 'y') or (prompt == 'yes') or (prompt == 'sure') or (prompt == 'true')  or (prompt == 'ja') : #Nice and thorough
 	print "What would you like to change it to?"
 	while True :
 		prompt = raw_input(">")
-		try :
+		try : #Will try to turn the input into a prompt
 			prompt = int(prompt)
 			break
-		except ValueError, e :
+		except ValueError, e : #If doing so raises a ValueError then instead of crashing the following is done:
 			print "Enter a number to be the new min value"
 	minval = prompt
 	print "Min value is now %s\n" %(minval)
@@ -44,12 +48,14 @@ if (prompt == '1') or (prompt == 'y') or (prompt == 'yes') or (prompt == 'sure')
 	print "What would you like to change it to?"
 	while True :
 		prompt = raw_input(">")
-		try :
+		try : #Will try to turn the input into a prompt
 			prompt = int(prompt)
 			break
-		except ValueError, e :
+		except ValueError, e : #If doing so raises a ValueError then instead of crashing the following is done:
 			print "Enter a number to be the new min value"
 	maxval = prompt
-	print "Min value is now %s" %(minval)
+	print "Min value is now %s\n" %(minval)
 else :
-	maxval = min(indata)
+	maxval = max(indata)
+	print ""
+print "Range of values is %s" %(maxval-minval)
